20a4ccf1f30985e1f5199b4348a70945cd714ae7
abcxj123 <abcxj123@naver.com>
2024-07-03 18:53:54 +0900

    ✨ 알림 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import "./App.css";
 import { Outlet } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { requestPermission } from "./firebaseConfig.ts";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import firebase from "firebase/app";
+import { AlarmCard } from "./components/index.ts";
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -13,18 +15,42 @@ const queryClient = new QueryClient({
 });
 
 function App() {
+  const [showAlarm, setShowAlarm] = useState<boolean>(false);
+  const [content, setContent] = useState({
+    title: "설문조사 요청 알림😁",
+    body: "모임은 즐거우셨나요?\n지금 당장 설문조사에 참여해 내 모임의 등급을\n올려보세요 ~!",
+    teamId: -1,
+  });
+
+  const toggleShowAlarm = (toggle: boolean) => {
+    setShowAlarm(toggle);
+  };
+
   requestPermission();
   useEffect(() => {
     if (firebase.messaging.isSupported()) {
       const messaging = firebase.messaging();
       messaging.onMessage((payload) => {
-        console.log(payload);
+        setContent({
+          title: payload.notification.title,
+          body: payload.notification.body,
+          teamId: payload.data.teamId,
+        });
+        setShowAlarm(true);
       });
     }
   }, []);
 
   return (
     <div className="min-h-screen">
+      {showAlarm && (
+        <AlarmCard
+          title={content.title}
+          body={content.body}
+          teamId={content.teamId}
+          toggleShowAlarm={toggleShowAlarm}
+        />
+      )}
       <QueryClientProvider client={queryClient}>
         <Outlet />
       </QueryClientProvider>
